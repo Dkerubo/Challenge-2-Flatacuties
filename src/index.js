4e431def83adb484a56a0592b5f68ae2cc8e9a42
root <root@Kerry>
2024-10-11 19:34:56 +0300

    git add . git commit -m

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 // Your code here
+// Base URL 
+const baseUrl = 'http://localhost:3000'
+document.addEventListener('DOMContentLoaded', () => {
+    const characterBar = document.getElementById('character-bar');
+    //fetch and display character names in #character-bar
+
+})
